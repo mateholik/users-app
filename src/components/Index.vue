eeff83556476363422b3a530c35cedbc0c30f978
mateholik <vaisov.vladimir@gmail.com>
2020-12-28 13:08:07 +0200

    vuex, router, axios, scss - added

---
+++ src/components/Index.vue
@@ -0,0 +1,23 @@
+<template>
+ <div class="test">
+   <p class="test__text">
+     Heloo
+   </p>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.test {
+  background: red;
+  &__text {
+    color: white;
+  }
+}
+</style>
